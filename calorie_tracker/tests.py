0404836f7e8e0c90b0144fef82eb04ba1505df2d
mushkorr <kinyaj25@gmail.com>
2026-05-26 15:02:12 +0300

    test: cover food item display text

--- calorie_tracker/tests.py
+++ calorie_tracker/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import FoodItem
+
+
+class FoodItemModelTests(TestCase):
+    def test_string_representation_includes_name_and_calories(self):
+        item = FoodItem.objects.create(name='Banana', calories=105)
+
+        self.assertEqual(str(item), 'Banana (105 kcal)')
